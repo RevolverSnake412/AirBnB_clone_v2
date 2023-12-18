19e0b149dc85d6f0cd31c120eea3ba005d90d925
RevolverSnake412 <oussag@hotmail.fr>
2023-12-18 16:09:30 +0000

    update

--- models/user.py
+++ models/user.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import *
-from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if models.storage_t == 'db':
         __tablename__ = 'users'
