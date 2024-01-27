1d5b10e918ef996870cd8d0fc89b192617158c99
RevolverSnake412 <oussag@hotmail.fr>
2024-01-27 18:00:55 +0000

    TASK 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""starts Flask web application"""
+
+if __name__ == '__main__':
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        return 'HBNB'
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def cisfun(text):
+        return 'C ' + text.replace('_', ' ')
+
+    @app.route('/python', strict_slashes=False)
+    @app.route('/python/<text>', strict_slashes=False)
+    def pythoniscool(text='is cool'):
+        return 'Python ' + text.replace('_', ' ')
+
+    @app.route('/number/<int:n>', strict_slashes=False)
+    def imanumber(n):
+        return "{:d} is a number".format(n)
+
+    app.run(host='0.0.0.0', port='5000')
